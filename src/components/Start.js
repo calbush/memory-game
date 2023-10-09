81935c90364966093d919e8b329f9dccf6b68535
Caleb Bush <calebmatthewbush@gmail.com>
2023-10-08 23:40:56 -0500

    Add Start component for initial load

---
+++ src/components/Start.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+import Instructions from '../components/Instructions'
+
+export default function Start(){
+    return(
+        <div>
+            <Header/>
+            <Instructions/>
+        </div>
+    )
+}
