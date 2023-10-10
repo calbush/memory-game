dcb44677f0427f89e79f7c3cc95890d046ac3b33
Caleb Bush <calebmatthewbush@gmail.com>
2023-10-09 23:08:37 -0500

    Simplify component

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,21 +1,5 @@
 export default function Instructions(){
     return(
-        <div>
-            <h2 className="h2">Rules:</h2>
-            <ul className="bullet-list">
-                <li>
-                    Select any of the reptiles.
-                </li>
-                <li>
-                    The reptile cards will be shuffled.
-                </li>
-                <li>
-                    Choose any reptile that you haven't already selected. If you select a reptile you've already clicked in a previous roun, the game will restart.
-                </li>
-                <li>
-                    See how high of a score you can get!
-                </li>
-            </ul>
-        </div>
+        <div>Test your memory!</div>
     )
 }
