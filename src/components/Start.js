17460a785f8844d3bd5543787cd0429865922772
Caleb Bush <calebmatthewbush@gmail.com>
2023-10-09 23:09:05 -0500

    Add Start component for initial page load

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,11 +1,12 @@
 import Header from '../components/Header'
 import Instructions from '../components/Instructions'
 
-export default function Start(){
+export default function Start({setGameState}){
     return(
         <div>
             <Header/>
             <Instructions/>
+            <button onClick={() => {setGameState('active')}}>Start Game</button>
         </div>
     )
 }
